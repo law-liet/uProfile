4551a8622d107c0a812426da40d2ec340f39fa59
Langxuan Su <lawliet@orion.codes>
2014-09-26 19:24:55 -0400

    First Submission

---
+++ test/person.swift
@@ -0,0 +1,13 @@
+//
+//  person.swift
+//  test
+//
+//  Created by chuan on 9/26/14.
+//  Copyright (c) 2014 Lalala. All rights reserved.
+//
+
+import Foundation
+
+class person: NSObject {
+    
+}
